98af0d2fae6731286b4e679caee3d0d46bc527b1
kronosGR <geo.elgeo@gmail.com>
2022-05-06 10:04:44 +0200

    about Part

--- src/Components/Parts/Home.jsx
+++ src/Components/Parts/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <span className={styles.title}>Hi I am Georgios</span>
-      <span className={styles.work}>... a Front-End Developet</span>
+      <span className={styles.work}>... a Front-End Developer</span>
       <span className={styles.love}>
         ... with passion for problem solving and creation
       </span>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../Components/Layout/Layout';
+import About from '../Components/Parts/About';
 import Home from '../Components/Parts/Home';
 
 const HomePage = () => {
@@ -7,6 +8,7 @@ const HomePage = () => {
     <div>
       <Layout>
         <Home />
+        <About />
       </Layout>
     </div>
   );
