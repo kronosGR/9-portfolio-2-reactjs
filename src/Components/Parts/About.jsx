9a72ca2e7044046542aaaccbb98a4072d1a535bc
kronosGR <geo.elgeo@gmail.com>
2022-05-12 09:41:32 +0200

    finished about

--- src/Components/Parts/About.jsx
+++ src/Components/Parts/About.jsx
@@ -8,23 +8,25 @@ const About = () => {
   return (
     <div id='about' className={styles.container}>
       <Title text='About' />
-      <img className={styles.image} src='/images/me.jpg' alt='kronos' />
-      <span className={styles.intro}>
-        Hi, I am Georgios Katsanakis, a Front-End Developer and a Greek living in Sandnes,
-        Norway.
-      </span>
+      <div className={styles.wrapper}>
+        <img className={styles.image} src='/images/me.jpg' alt='kronos' />
+        <span className={styles.intro}>
+          Hi, I am Georgios Katsanakis, a Front-End Developer and a Greek living in
+          Sandnes, Norway.
+        </span>
 
-      <span className={styles.details}>
-        I am passionate on everything I do and I give 100%. I love to create and solve
-        problems. In my free time I enjoy spending time with my family, improving myself
-        by practicing and learning new technologies and Unreal Engine.
-      </span>
-      <span className={styles.quote}>"Never stop learning"</span>
-      <span className={styles.quote}>
-        "It is the definition of paranoia to do the same thing and expect a different
-        result. "
-      </span>
-      <Skills />
+        <span className={styles.details}>
+          I am passionate on everything I do and I give 100%. I love to create and solve
+          problems. In my free time I enjoy spending time with my family, improving myself
+          by practicing and learning new technologies and Unreal Engine.
+        </span>
+        <span className={styles.quote}>"Never stop learning"</span>
+        <span className={styles.quote}>
+          "It is the definition of paranoia to do the same thing and expect a different
+          result. "
+        </span>
+        <Skills />
+      </div>
     </div>
   );
 };
